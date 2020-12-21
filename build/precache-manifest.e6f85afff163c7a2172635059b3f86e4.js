29341e38a10f958adb0317de952b3ecb3586c44a
israx <70438514+israx@users.noreply.github.com>
2020-12-21 10:51:55 -0500

    build

---
+++ build/precache-manifest.e6f85afff163c7a2172635059b3f86e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdcb7b7466c97a171b5335970b375eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6546b4a19788f851038",
+    "url": "/static/css/main.6fc6ec1e.chunk.css"
+  },
+  {
+    "revision": "af6682bd124f516ed152",
+    "url": "/static/js/2.95e4c309.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.95e4c309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6546b4a19788f851038",
+    "url": "/static/js/main.f2ccb625.chunk.js"
+  },
+  {
+    "revision": "81d08e04a2e4215dd2ef",
+    "url": "/static/js/runtime-main.5856509f.js"
+  }
+]);
